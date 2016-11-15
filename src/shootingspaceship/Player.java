a4e98140d4e68f4b6f3995bdb326ba565d80637e
oki6551 <oki6551@naver.com>
2016-11-15 18:15:11 +0900

    Update Player.java

--- src/shootingspaceship/Player.java
+++ src/shootingspaceship/Player.java
@@ -18,18 +18,28 @@ public class Player {
     private int y_pos;
     private int min_x;
     private int max_x;
+    private int min_y;
+    private int max_y;
 
-    public Player(int x, int y, int min_x, int max_x) {
+    public Player(int x, int y, int min_x, int max_x, int min_y, int max_y) {
         x_pos = x;
         y_pos = y;
         this.min_x = min_x;
         this.max_x = max_x;
+        this.min_y = min_y;
+        this.max_y = max_y;
     }
 
     public void moveX(int speed) {
         x_pos += speed;
         if( x_pos < min_x) x_pos = min_x;
         if( x_pos > max_x) x_pos = max_x;
+    }
+    
+    public void moveY(int speed) {
+        y_pos += speed;
+        if( y_pos < min_y ) y_pos = min_y;
+        if( y_pos > max_y ) y_pos = max_y;
     }
 
     public int getX() {
@@ -40,8 +50,8 @@ public class Player {
         return y_pos;
     }
 
-    public Shot generateShot() {
-        Shot shot = new Shot(x_pos, y_pos);
+    public ShotType generateShot(int damage) {
+        ShotType shot = new ShotType(x_pos, y_pos, damage);
 
         return shot;
     }
