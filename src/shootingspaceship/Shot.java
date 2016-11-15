1feb956752bf946bdea3ee3c8cad6ef073c3ffe5
oki6551 <oki6551@naver.com>
2016-11-15 18:15:30 +0900

    Update Shot.java

--- src/shootingspaceship/Shot.java
+++ src/shootingspaceship/Shot.java
@@ -11,11 +11,30 @@ import java.awt.Color;
  *
  * @author wgpak
  */
+
+class ShotType extends Shot {
+    private int damage;
+
+    public ShotType(int x, int y, int damage) {
+        super(x, y);
+        this.damage = damage;
+    }
+    public int getDamage() {
+        return damage;
+    }
+    public int collided(int healthPoint) {
+        alive = false;
+        x_pos = 0;
+        y_pos = 0;
+        return (healthPoint - damage);
+    }
+}
+
 public class Shot {
 
-    private int x_pos;
-    private int y_pos;
-    private boolean alive;
+    protected int x_pos;
+    protected int y_pos;
+    protected boolean alive;
     private final int radius = 3;
 
     public Shot(int x, int y) {
@@ -36,15 +55,33 @@ public class Shot {
         y_pos += speed;
     }
 
-    public void drawShot(Graphics g) {
+    public void drawShot(Graphics g, int currentShot) {
         if (!alive) {
             return;
         }
-        g.setColor(Color.yellow);
-        g.fillOval(x_pos, y_pos, radius, radius);
+        switch(currentShot) {
+            case 0:
+                g.setColor(Color.yellow);
+                g.fillOval(x_pos, y_pos, radius, radius);
+                break;
+            case 1:
+                g.setColor(Color.red);
+                g.fillOval(x_pos, y_pos, radius, radius);
+                break;
+            case 2:
+                g.setColor(Color.blue);
+                g.fillOval(x_pos, y_pos, radius, radius);
+                break;
+            case 3:
+                g.setColor(Color.pink);
+                g.fillOval(x_pos, y_pos, radius, radius);
+                break;
+        }
     }
 
     public void collided() {
         alive = false;
+        x_pos = 0;
+        y_pos = 0;
     }
 }
